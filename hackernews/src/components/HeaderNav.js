c7d1e0f500c679e1a98ee0763167cc0943166f9c
jnelson <jennifer.nelson242@gmail.com>
2021-09-30 23:52:30 -0500

    styled navbar, fixed allignment bug

--- hackernews/src/components/HeaderNav.js
+++ hackernews/src/components/HeaderNav.js
@@ -4,15 +4,16 @@ import{Navbar, Nav, Container} from "react-bootstrap";
 
 function Headernav(){
     return(
-        <Navbar>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
             <Navbar.Brand as={Link} to="/">Hacker News</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav"/>
-             <Nav className="me-auto">
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
                 <Nav.Link as={Link} to="/">Search the News</Nav.Link>
                 <Nav.Link as={Link} to="/history">View Search History</Nav.Link>
              </Nav>
+            </Navbar.Collapse>
             </Container>
         </Navbar>
     )
