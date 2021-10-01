37cb36499e8012f737a55be2400820c10f5da10a
jnelson <jennifer.nelson242@gmail.com>
2021-09-30 22:32:42 -0500

    pages drafted

---
+++ hackernews/src/pages/History.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function History(){
+    return(
+        <>
+        <h1>Hello World!</h1>
+        <p>Where's your history?</p>
+        </>
+    );
+}
+
+export default History;
---
+++ hackernews/src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search(){
+    return(
+        <>
+        <h1>Hello World</h1>
+        <p>Time to search for stuff!!!</p>
+        </>
+    )
+}
+
+export default Search;
