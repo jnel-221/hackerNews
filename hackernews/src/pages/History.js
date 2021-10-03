21f0368aedd180aeeec7e2368b4c905d0267fb06
jnelson <jennifer.nelson242@gmail.com>
2021-10-03 18:40:54 -0500

    added button to clear search history

--- hackernews/src/pages/History.js
+++ hackernews/src/pages/History.js
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import Headernav from "../components/HeaderNav";
 import List from "../components/List";
 
 function History() {
   //no 'setting' happens on this page, removed setter from useState hook.
-  const [list] = useState(() => {
+  const [list, setList] = useState(() => {
     const saved = sessionStorage.getItem("mySearches");
     const savedSearches = JSON.parse(saved);
     return savedSearches || [];
   });
+
+  function handleClick(e){
+    e.preventDefault();
+    sessionStorage.clear();
+    setList([]);
+  }
 
   return (
     <>
       <Headernav />
       <h2 className="m-3">Search History</h2>
       <List savedSearches={list} />
+      <Button variant="warning" className="m-3" type="submit" onClick={handleClick}>Clear History</Button>
     </>
   );
 }
