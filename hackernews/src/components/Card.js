57ca65d3c28455103629f09e88e1b8f909a3cb4d
jnelson <jennifer.nelson242@gmail.com>
2021-10-02 19:02:11 -0500

    search values stored in array, however storing as compiling strings. Need to determine why

--- hackernews/src/components/Card.js
+++ hackernews/src/components/Card.js
@@ -1,25 +1,23 @@
 import React from "react";
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
-
-function Cards({results}){
-
-    return(
-        <>
-        {results.map((result,i) => (
-        <Card className="my-3">
-        
-            <Card.Body key={i}>
-                <Card.Title>{result.title}</Card.Title>
-                <Card.Subtitle>by {result.author}</Card.Subtitle>
-                <Card.Subtitle class="mt-1">created {result.date}</Card.Subtitle>
-                <Card.Link href={result.url} target="_blank">View Story</Card.Link>
-            </Card.Body>
-        
+function Cards({ results }) {
+  return (
+    <>
+      {results.map((result, i) => (
+        <Card className="my-3" key={i}>
+          <Card.Body>
+            <Card.Title>{result.title}</Card.Title>
+            <Card.Subtitle>by {result.author}</Card.Subtitle>
+            <Card.Subtitle className="mt-1">created {result.date}</Card.Subtitle>
+            <Card.Link href={result.url} target="_blank">
+              View Story
+            </Card.Link>
+          </Card.Body>
         </Card>
-        ))}
-        </>
-    );
+      ))}
+    </>
+  );
 }
 
 export default Cards;
--- hackernews/src/pages/Search.js
+++ hackernews/src/pages/Search.js
@@ -1,83 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Headernav from "../components/HeaderNav";
 import { InputGroup, FormControl, Button } from "react-bootstrap";
 import axios from "axios";
 import Cards from "../components/Card";
 import FormatDate from "../utils/helpers/formatDate.js";
 
-
 function Search() {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
-  const [history, setHistory] = useState([]);
-  
+  useEffect(function storeSearch(){
+    let history = JSON.parse(sessionStorage.getItem("mySearches"))||[];
+     
+    if(search !== ""){
+        
+        history = [...history, search];
 
+        sessionStorage.setItem("mySearches",JSON.stringify(history))
+        console.log("useEffect history has what: ", history);
+    }
+
+  })
+
+
+  const [results, setResults] = useState([]);
+//   const [history, setHistory] = useState([]);
+
+ 
   function handleSubmit(e) {
     e.preventDefault();
 
-    console.log("You clicked submit! ", search);
-
+    setSearch(e.target.value.trim());    
     if (search !== "") {
       axios
         .get(`http://hn.algolia.com/api/v1/search?query=${search}&tags=story`)
         .then((res) => {
           const stories = res.data.hits;
-          
+
           setResults(stories);
-          setResults((stories)=>{
-              console.log("updater ",stories)
-              return stories;
+          setResults((stories) => {
+            return stories;
           });
           parseHistory(stories);
-          
         });
     } else {
       console.log("enter search term to get results");
     }
 
-    setHistory((history) => [...history, search]);
-    setHistory((history) => {
-        return history;
-    })
-    setStorage(history);
-   
-    console.log(
-      "handleSubmit history ",
-      history,
-      ` history is ${history.length} items long`
-    );
-   
   }
 
-  function parseHistory(arr){
-    setResults(arr.map((story)=> {
-        return{
-            title: story.title,
-            author: story.author,
-            url: story.url,
-            date: FormatDate(story.created_at),
+  
+
+  function parseHistory(arr) {
+    setResults(
+      arr.map((story) => {
+        return {
+          title: story.title,
+          author: story.author,
+          url: story.url,
+          date: FormatDate(story.created_at),
         };
-        
-    }))
-  }
-  console.log("parseHistory: ", results);
-
-  function setStorage(array) {
-    let json = JSON.stringify(array);
-    console.log("setStorage history arr to string contains ", json);
-    sessionStorage.setItem("searchHistory",json);
+      })
+    );
   }
 
-//   function handleChange(e) {
-//     e.preventDefault();
-//     setSearch(e.target.value.trim());
-//   }
 
   return (
     <>
       <Headernav />
       <InputGroup
-        className="mt-3 mx-auto"
+        className="mt-3 mb-4 mx-auto"
         id="search-term"
         value={search}
         onChange={(e) => setSearch(e.target.value.trim())}
@@ -96,9 +86,9 @@ function Search() {
           Search
         </Button>
       </InputGroup>
-      <p>Hey, looks like you're searching for {search}!</p>
+      {/* <p>Hey, looks like you're searching for {search}!</p> */}
 
-      <Cards results={results}/>
+      <Cards results={results} />
     </>
   );
 }
