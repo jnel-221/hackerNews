61d74bdd9fee433abb6f7ac7a3a1a53c9a2d221d
jnelson <jennifer.nelson242@gmail.com>
2021-10-03 23:41:56 -0500

    removed comments

--- hackernews/src/pages/History.js
+++ hackernews/src/pages/History.js
@@ -4,7 +4,6 @@ import Headernav from "../components/HeaderNav";
 import List from "../components/List";
 
 function History() {
-  //no 'setting' happens on this page, removed setter from useState hook.
   const [list, setList] = useState(() => {
     const saved = sessionStorage.getItem("mySearches");
     const savedSearches = JSON.parse(saved);
